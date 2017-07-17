e6008b60ba271f2b9bf6e7bc31d693c35b752f28
infomericaincblr <niranjan@infomericainc.com>
2017-07-17 11:57:16 +0530

    commit phase 1

---
+++ sample1/src/main/java/com/infomericainc/sample1/App.java
@@ -0,0 +1,14 @@
+package com.infomericainc.sample1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("hello changes");
+    }
+}
